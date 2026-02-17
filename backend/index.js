c88de7eadde28f59bc19fb277acb33eace96371f
vedikaagrawal464 <vedika1464@gmail.com>
2026-02-17 13:12:34 +0530

    Backend: initial arXiv paper search for ResearchLens

--- backend/index.js
+++ backend/index.js
@@ -5,18 +5,30 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+// Home route (just to test server)
+app.get("/", (req, res) => {
+  res.send("ResearchLens Backend is running");
+});
+
+// Search research papers
 app.get("/search", async (req, res) => {
   const query = req.query.q;
 
+  if (!query) {
+    return res.status(400).send("Query is required");
+  }
+
+  const url = `http://export.arxiv.org/api/query?search_query=all:${query}&start=0&max_results=5`;
+
   try {
-    const url = `http://export.arxiv.org/api/query?search_query=all:${query}&start=0&max_results=5`;
     const response = await axios.get(url);
     res.send(response.data);
-  } catch {
-    res.status(500).send("Error fetching papers");
+  } catch (error) {
+    res.status(500).send("Error fetching research papers");
   }
 });
 
+// Start server
 app.listen(5000, () => {
-  console.log("ResearchLens backend running");
+  console.log("ResearchLens backend running on port 5000");
 });
